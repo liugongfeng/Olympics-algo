b7338a1b00e1387b6d0057744b8af11ec77c0f28
liugongfeng <liugong.feng@aliyun.com>
2018-06-10 09:13:21 +0800

    Add files via upload

---
+++ ch01/main9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    /** ex1-1  year */
+    int year;
+    scanf("%d", &year );
+    if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        printf("yes\n");
+    else printf("no\n");
+
+    return 0;
+}
