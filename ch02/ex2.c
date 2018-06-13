6895e47d604b3f41cd3d60c17160ce75e4072a85
liugongfeng <liugong.feng@aliyun.com>
2018-06-13 21:33:36 +0800

    Add files via upload

---
+++ ch02/ex2.c
@@ -0,0 +1,21 @@
+//
+// Created by lgf on 2018/6/13.
+//
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    for (int a=1; a<=9; a++)
+        for (int b=0; b<=9; b++)
+        {
+            int n = a*1100 + b*11;
+            int m = floor(sqrt(n) + 0.5);
+            if (m*n == n)
+            printf("%d\n", n);
+        }
+    return 0;
+
+}
+
+
